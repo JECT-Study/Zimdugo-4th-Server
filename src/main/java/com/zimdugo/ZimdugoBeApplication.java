245120ffef2bf248ecdca1a78369af5bbb7b0267
yongckim <zxcv9203@naver.com>
2026-03-01 17:55:34 +0900

    :wrench: chore : editconfig 설정 추가
    - 코드 스타일 일관성을 위한 편집기 공통 설정
    - Java 120자 라인 제한, YAML/JSON 2칸 인덴트
    - Markdown trailing whitespace 보존
    - Gradle wrapper 줄바꿈 처리

--- src/main/java/com/zimdugo/ZimdugoBeApplication.java
+++ src/main/java/com/zimdugo/ZimdugoBeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ZimdugoBeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZimdugoBeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZimdugoBeApplication.class, args);
+    }
 
 }
