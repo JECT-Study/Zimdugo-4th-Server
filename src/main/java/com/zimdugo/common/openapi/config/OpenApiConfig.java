cfbb6a0463aa421c8a04d74f466a96a2f9ba257c
mike7643 <developerJYumin@gmail.com>
2026-04-28 21:07:49 +0900

    fix: Swagger 서버 URL을 HTTPS로 명시

--- src/main/java/com/zimdugo/common/openapi/config/OpenApiConfig.java
+++ src/main/java/com/zimdugo/common/openapi/config/OpenApiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,6 +20,8 @@ public class OpenApiConfig {
             .info(new Info()
                 .title("Zimdugo API")
                 .description("Zimdugo 백엔드 API 문서"))
+            .addServersItem(new Server().url("https://api.zimdugo.com").description("Production"))
+            .addServersItem(new Server().url("http://localhost:8080").description("Local"))
             .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
             .components(new Components()
                 .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
