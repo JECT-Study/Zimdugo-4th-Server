0651b0477ef582cf448d759ddd3f0fe10fe00dd6
mike7643 <developerJYumin@gmail.com>
2026-06-09 16:08:33 +0900

    feat: Swagger에서 인증 관련 어노테이션 파라미터 제외 설정

--- src/main/java/com/zimdugo/common/openapi/config/OpenApiConfig.java
+++ src/main/java/com/zimdugo/common/openapi/config/OpenApiConfig.java
@@ -6,11 +6,19 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.utils.SpringDocUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
+    static {
+        SpringDocUtils.getConfig().addAnnotationsToIgnore(
+            com.zimdugo.common.security.CurrentUser.class,
+            com.zimdugo.common.security.NullableCurrentUser.class
+        );
+    }
 
     private static final String SECURITY_SCHEME_NAME = "bearerAuth";
 
