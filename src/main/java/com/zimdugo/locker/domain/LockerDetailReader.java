9f913bdb336e2b7c620ea0de5a0c10a65c49ced5
mike7643 <developerJYumin@gmail.com>
2026-06-14 10:51:46 +0900

    feat: 보관함 상세 및 장소 목록 리더 포트 인터페이스에 다국어 적용

--- src/main/java/com/zimdugo/locker/domain/LockerDetailReader.java
+++ src/main/java/com/zimdugo/locker/domain/LockerDetailReader.java
@@ -3,5 +3,9 @@ package com.zimdugo.locker.domain;
 import java.util.Optional;
 
 public interface LockerDetailReader {
-    Optional<LockerDetail> readById(Long lockerId);
+    Optional<LockerDetail> readById(Long lockerId, Long userId, String languageCode);
+
+    default Optional<LockerDetail> readById(Long lockerId) {
+        return readById(lockerId, null, "ko");
+    }
 }
