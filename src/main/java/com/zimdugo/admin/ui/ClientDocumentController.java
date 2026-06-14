5009b22fe4e6946ab7e929e023199a7642d4ca9b
mike7643 <developerJYumin@gmail.com>
2026-06-14 10:51:41 +0900

    feat: 관리자 문서 번역 관리 API 컨트롤러 및 클라이언트 조회 컨트롤러 개선

--- src/main/java/com/zimdugo/admin/ui/ClientDocumentController.java
+++ src/main/java/com/zimdugo/admin/ui/ClientDocumentController.java
@@ -3,13 +3,15 @@ package com.zimdugo.admin.ui;
 import com.zimdugo.admin.application.AdminDocumentService;
 import com.zimdugo.admin.domain.DocumentType;
 import com.zimdugo.admin.ui.dto.ClientDocumentResponse;
+import com.zimdugo.common.i18n.AcceptLanguageResolver;
 import com.zimdugo.core.response.RestResponse;
 import com.zimdugo.core.response.SuccessCode;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,14 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ClientDocumentController {
 
     private final AdminDocumentService adminDocumentService;
+    private final AcceptLanguageResolver acceptLanguageResolver;
 
     @GetMapping
     public ResponseEntity<RestResponse<List<ClientDocumentResponse>>> getActiveDocuments(
-        @RequestParam(name = "type") DocumentType type
+        @RequestParam(name = "type") DocumentType type,
+        @RequestHeader(name = HttpHeaders.ACCEPT_LANGUAGE, required = false) String acceptLanguage
     ) {
-        List<ClientDocumentResponse> responses = adminDocumentService.getActiveDocumentsByType(type).stream()
-            .map(ClientDocumentResponse::new)
-            .collect(Collectors.toList());
+        List<ClientDocumentResponse> responses = adminDocumentService.getLocalizedActiveDocumentsByType(
+            type,
+            acceptLanguageResolver.resolve(acceptLanguage)
+        );
         return ResponseEntity.ok(RestResponse.of(SuccessCode.OK, responses));
     }
 }
