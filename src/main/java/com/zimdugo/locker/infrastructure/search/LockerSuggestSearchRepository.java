056ec1372449b0916452a29b7ac2793bdcc87009
Yumin Jung <121170730+mike7643@users.noreply.github.com>
2026-06-14 15:10:57 +0900

    Revert "feat: 엘라스틱서치 인덱스 동기화 서비스 및 검색 레포지토리 다국어 대응 로직"
    This reverts commit 55d8bf9caea91415cba7f55db1bc90a2d5235b5e.

--- src/main/java/com/zimdugo/locker/infrastructure/search/LockerSuggestSearchRepository.java
+++ src/main/java/com/zimdugo/locker/infrastructure/search/LockerSuggestSearchRepository.java
@@ -1,8 +1,6 @@
 package com.zimdugo.locker.infrastructure.search;
 
-import java.util.List;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface LockerSuggestSearchRepository extends ElasticsearchRepository<LockerSuggestDocument, String> {
-    void deleteByPlaceIdIn(List<Long> placeIds);
 }
