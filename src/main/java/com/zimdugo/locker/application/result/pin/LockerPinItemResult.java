2452ee898c62ac9d348e8b7caf2efc04249277f3
mike7643 <developerJYumin@gmail.com>
2026-06-24 23:08:00 +0900

    feat: 지도 핀 조회 결과 및 응답 모델에 즐겨찾기 여부와 하위 보관함 개수 필드 추가

--- src/main/java/com/zimdugo/locker/application/result/pin/LockerPinItemResult.java
+++ src/main/java/com/zimdugo/locker/application/result/pin/LockerPinItemResult.java
@@ -5,13 +5,15 @@ public record LockerPinItemResult(
     Long placeId,
     Long lockerId,
     double latitude,
-    double longitude
+    double longitude,
+    Boolean isFavorite,
+    Integer lockerCount
 ) {
-    public static LockerPinItemResult place(Long placeId, double latitude, double longitude) {
-        return new LockerPinItemResult(LockerPinType.PLACE, placeId, null, latitude, longitude);
+    public static LockerPinItemResult place(Long placeId, double latitude, double longitude, int count) {
+        return new LockerPinItemResult(LockerPinType.PLACE, placeId, null, latitude, longitude, null, count);
     }
 
-    public static LockerPinItemResult locker(Long lockerId, double latitude, double longitude) {
-        return new LockerPinItemResult(LockerPinType.LOCKER, null, lockerId, latitude, longitude);
+    public static LockerPinItemResult locker(Long lockerId, double latitude, double longitude, boolean isFavorite) {
+        return new LockerPinItemResult(LockerPinType.LOCKER, null, lockerId, latitude, longitude, isFavorite, null);
     }
 }
