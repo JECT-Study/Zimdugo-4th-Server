77e3a8b5efe6caa981226938bdefd34db08bb84d
mike7643 <developerJYumin@gmail.com>
2026-08-27 21:08:30 +0900

    refactor: 요청 언어 해석 경로 통합

--- src/main/java/com/zimdugo/common/i18n/CurrentRequestLanguage.java
+++ src/main/java/com/zimdugo/common/i18n/CurrentRequestLanguage.java
@@ -1,18 +1,32 @@
 package com.zimdugo.common.i18n;
 
-import java.util.Locale;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Component
+@RequiredArgsConstructor
 public class CurrentRequestLanguage {
 
+    private final AcceptLanguageResolver acceptLanguageResolver;
+
     public SupportedLanguage resolve() {
-        Locale locale = LocaleContextHolder.getLocale();
-        if (locale == null) {
-            return SupportedLanguage.ENGLISH;
+        HttpServletRequest request = currentRequest();
+        if (request != null) {
+            return acceptLanguageResolver.resolve(request.getHeader("Accept-Language"));
         }
-        return AcceptLanguageResolver.parseLanguage(locale.toLanguageTag())
-            .orElse(SupportedLanguage.ENGLISH);
+        return acceptLanguageResolver.resolve(LocaleContextHolder.getLocale().toLanguageTag());
+    }
+
+    private HttpServletRequest currentRequest() {
+        RequestAttributes attributes = RequestContextHolder.getRequestAttributes();
+        if (attributes instanceof ServletRequestAttributes servletAttributes) {
+            return servletAttributes.getRequest();
+        }
+        return null;
     }
 }
