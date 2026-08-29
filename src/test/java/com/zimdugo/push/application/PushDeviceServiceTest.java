26cdb74bb933d0a0c797a6a30cbff1996b1c55fe
Yumin Jung <121170730+mike7643@users.noreply.github.com>
2026-08-29 13:33:50 +0900

    Revert "fix: 푸시 기기 테스트 미사용 import 제거"
    This reverts commit b7194f241cac2789b257623494e23e3e7155920e.

--- src/test/java/com/zimdugo/push/application/PushDeviceServiceTest.java
+++ src/test/java/com/zimdugo/push/application/PushDeviceServiceTest.java
@@ -2,6 +2,7 @@ package com.zimdugo.push.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.zimdugo.push.domain.PushDeviceReader;
 import com.zimdugo.push.domain.PushDeviceStore;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
