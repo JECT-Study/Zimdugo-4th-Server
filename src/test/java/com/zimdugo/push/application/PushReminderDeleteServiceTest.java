5fd18c92ddd8d1f2c581175f3efc972d541b5cd1
mike7643 <developerJYumin@gmail.com>
2026-09-02 00:22:32 +0900

    test: 리마인더 삭제 테스트 import 정리

--- src/test/java/com/zimdugo/push/application/PushReminderDeleteServiceTest.java
+++ src/test/java/com/zimdugo/push/application/PushReminderDeleteServiceTest.java
@@ -12,7 +12,6 @@ import java.time.ZoneOffset;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
